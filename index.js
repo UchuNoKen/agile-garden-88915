e259edcc014624e66541a6a3a4b267240171d135
UchuNoKen <xen.o.fite@gmail.com>
2019-02-17 12:57:30 -0500

    Added express routes handlers.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+// Import express w/require using CommonJS modules
 const express = require("express");
+// Create an express app by calling express function
 const app = express();
+
+// root route handler
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+app.listen(5000);
+
+// app represents underlying express server
+// get creates a new route handler

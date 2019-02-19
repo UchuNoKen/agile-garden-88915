ec82d91d84c96d660efd4db089bb02daed62b60c
UchuNoKen <xen.o.fite@gmail.com>
2019-02-19 11:37:06 -0500

    Comments.

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const express = require("express");
 // Create an express app by calling express function
 const app = express();
 
-// root route handler
+// app - represents underlying express server
+// get - creates a new route handler
+// / - root route handler
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-// app represents underlying express server
-// get creates a new route handler

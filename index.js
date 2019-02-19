a84b5bd567815cf08e1802e91a863cc73fac9eb7
UchuNoKen <xen.o.fite@gmail.com>
2019-02-19 18:07:52 -0500

    passport boilerplate.

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-// Import express w/require using CommonJS modules
+// Import express w/require using CommonJS modules'
+// require passport
+// require passport-google-oauth20
 const express = require("express");
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 // Create an express app by calling express function
 const app = express();
 
+// tell passport to use new instance of GoogleStrategy
+passport.use(new GoogleStrategy());
+
 // app - represents underlying express server
 // get - creates a new route handler
 // / - root route handler
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+// app.get("/", (req, res) => {
+//   res.send({ hi: "there" });
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);

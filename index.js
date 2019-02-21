3f03c79326215b8d778232155f26f4cf042011c4
UchuNoKen <xen.o.fite@gmail.com>
2019-02-21 08:01:02 -0500

    added route handler for OAuth callback.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-// Import express w/require using CommonJS modules'
+// require express using CommonJS modules
 // require passport
 // require passport-google-oauth20
+// require keys
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
@@ -9,13 +10,35 @@ const keys = require("./config/keys");
 // Create an express app by calling express function
 const app = express();
 
-// tell passport to use new instance of GoogleStrategy
+//  tell passport to use new instance of GoogleStrategy
+//  passed object with ID and secret to new GoogleStrategy()
+//  passed third configuration option of callbackURL
+//  added second parameter, callback function with accessToken parameter
 passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+// route handler for /auth/google
+// GoogleStrategy has an internal identifier of string 'google',
+// passport associates authenicating using GoogleStrategy with the string
+// 'profile' and 'email' are the scopes that we request from google
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
   })
 );
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 // app - represents underlying express server
 // get - creates a new route handler
@@ -25,4 +48,6 @@ passport.use(
 // });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`App running at port ${PORT}`);
+});

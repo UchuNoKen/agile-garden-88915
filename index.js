155892949c2e7768dd19737890bb760fd4e58274
UchuNoKen <xen.o.fite@gmail.com>
2019-02-21 06:58:28 -0500

    added google clientID.

--- index.js
+++ index.js
@@ -4,12 +4,18 @@
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 // Create an express app by calling express function
 const app = express();
 
 // tell passport to use new instance of GoogleStrategy
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret
+  })
+);
 
 // app - represents underlying express server
 // get - creates a new route handler

63c00074633140589ef259507fff534badb701e9
UchuNoKen <xen.o.fite@gmail.com>
2019-02-21 15:39:54 -0500

    created mongoose schema

--- services/passport.js
+++ services/passport.js
@@ -3,7 +3,10 @@
 // require keys
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
 
 //  tell passport to use new instance of GoogleStrategy
 //  passed object with ID and secret to new GoogleStrategy()
@@ -17,9 +20,10 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken);
-      console.log("refresh token", refreshToken);
-      console.log("profile: ", profile);
+      // console.log("access token", accessToken);
+      // console.log("refresh token", refreshToken);
+      // console.log("profile: ", profile);
+      new User({ googleId: profile.id }).save();
     }
   )
 );
